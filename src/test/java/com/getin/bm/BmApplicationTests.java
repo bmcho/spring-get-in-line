73359a754d0b3ea9358adbd4f309dcea3bc7b324
bmcho <bmcho@naver.com>
2022-06-13 16:03:27 +0900

    spring setup
    spring setup

---
+++ src/test/java/com/getin/bm/BmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getin.bm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
