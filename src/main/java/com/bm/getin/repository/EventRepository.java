0ce365331aded7c7cc6bf9869d9fe15af21c69a0
bmcho <bmcho91@naver.com>
2022-07-30 23:04:28 +0900

    Implement #12 - EventRepository
    querydsl의 기능에 맞게 수정

--- src/main/java/com/bm/getin/repository/EventRepository.java
+++ src/main/java/com/bm/getin/repository/EventRepository.java
@@ -1,9 +1,26 @@
 package com.bm.getin.repository;
 
 import com.bm.getin.domain.Event;
+import com.bm.getin.domain.QEvent;
+import com.querydsl.core.types.dsl.ComparableExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 public interface EventRepository extends
-        JpaRepository<Event, Long> {
+        JpaRepository<Event, Long>,
+        QuerydslPredicateExecutor<Event>,
+        QuerydslBinderCustomizer<QEvent> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QEvent root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.placeId, root.eventName, root.eventStatus, root.eventStartDatetime, root.eventEndDatetime);
+        bindings.bind(root.eventName).first(StringExpression::likeIgnoreCase);
+        bindings.bind(root.eventStartDatetime).first(ComparableExpression::goe);
+        bindings.bind(root.eventEndDatetime).first(ComparableExpression::loe);
+    }
 
 }
