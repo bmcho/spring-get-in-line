fff36f0ca16f5880b9b832877613ebd8fdf027f6
bmcho <bmcho91@naver.com>
2022-07-30 20:22:29 +0900

    implement #12 - domain entities
    각 도메인 마다 본격적으로 `@Entity` 를 지정하고
    JAP 기술 사용에 필요한 세부 요소를 적용
    
    * `getter()` 는 전체 적용
    * `setter()` 는 선택적으로 적용 (`id`, `created_at` 등은 임의로 세팅하지 못하도록)
    * 편리한 검사와 관찰을 위해 `@ToString` `@EqualsAndHashCode` 적용
    * 인덱스 표현
    * Auditable Entity 적용
    * ID의 설정
    * `created_at`, `modified_at` 의 처리 방법 설정
    * 기본 생성자 작성 - JPA 프레임워크 용이므로 열지 않고 `protected`
    * 데이터 생성자 작성 - `of()`를 사용하게끔 `protected`

--- src/main/java/com/bm/getin/domain/AdminPlaceMap.java
+++ src/main/java/com/bm/getin/domain/AdminPlaceMap.java
@@ -1,18 +1,62 @@
 package com.bm.getin.domain;
 
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import net.minidev.json.annotate.JsonIgnore;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@ToString
+@EqualsAndHashCode
+@Table(indexes = {
+        @Index(columnList = "adminId"),
+        @Index(columnList = "placeId"),
+        @Index(columnList = "createAt"),
+        @Index(columnList = "modifiedAt")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class AdminPlaceMap {
 
+    @JsonIgnore
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private Integer adminId;
+    @Setter
+    @Column(nullable = false)
+    private Long adminId;
 
-    private Integer placeId;
+    @Setter
+    @Column(nullable = false)
+    private Long placeId;
 
+    @Column(nullable = false, insertable = false, updatable = false,
+            columnDefinition = "datetime default CURRENT_TIMESTAMP")
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @Column(nullable = false, insertable = false, updatable = false,
+            columnDefinition = "datetime default CURRENT_TIMESTAMP")
+    @LastModifiedDate
     private LocalDateTime modifiedAt;
+
+    protected AdminPlaceMap() {
+    }
+
+    protected AdminPlaceMap(Long adminId, Long placeId) {
+        this.adminId = adminId;
+        this.placeId = placeId;
+    }
+
+    public static AdminPlaceMap of(Long adminId, Long placeId) {
+        return new AdminPlaceMap(adminId, placeId);
+    }
 }
