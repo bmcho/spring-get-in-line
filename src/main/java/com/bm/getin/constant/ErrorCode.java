3f7eb0c6f3e3ee75b7201b4ab26ff25ed4d52c8f
bmcho <bmcho91@naver.com>
2022-08-02 00:08:21 +0900

    Update - ErrorCode to have HttpStatus
    기존에는 `ErrorCode` 로부터
    적당한 http status code를 선택하는 것을
    에러 핸들러에게 맡겼김
    `HttpStatus`는 스프링 클래스이고 이것을
    `ErrorCode` 안에 넣고 싶지 않았기 때문
    
    근데 넣기로 결정.
    이 편이 http status code를 관리하는 방법을
    더욱 편하게 해줄 것.
    이와 더불어 `HttpStatus`로 `ErrorCode` 만드는
    로직도  추가
    
    따라서 `ErrorCode` 안에 `ErrorCategory`는
    필요 없어짐

--- src/main/java/com/bm/getin/constant/ErrorCode.java
+++ src/main/java/com/bm/getin/constant/ErrorCode.java
@@ -1,27 +1,49 @@
 package com.bm.getin.constant;
 
+import com.bm.getin.exception.GeneralException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.Predicate;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    OK(0, ErrorCategory.NORMAL, "Ok"),
+    OK(0, HttpStatus.OK, "Ok"),
 
-    BAD_REQUEST(10000, ErrorCategory.CLIENT_SIDE, "Bad request"),
-    SPRING_BAD_REQUEST(10001, ErrorCategory.CLIENT_SIDE, "Spring-detected bad request"),
-    VALIDATION_ERROR(10002, ErrorCategory.CLIENT_SIDE, "Validation error"),
-    NOT_FOUND(10003, ErrorCategory.CLIENT_SIDE, "Requested resource is not found"),
-    INTERNAL_ERROR(20000, ErrorCategory.SERVER_SIDE, "Internal error"),
-    SPRING_INTERNAL_ERROR(20001, ErrorCategory.SERVER_SIDE, "Spring-detected internal error"),
-    DATA_ACCESS_ERROR(20002, ErrorCategory.SERVER_SIDE, "Data access error");
+    BAD_REQUEST(10000, HttpStatus.BAD_REQUEST, "Bad request"),
+    SPRING_BAD_REQUEST(10001, HttpStatus.BAD_REQUEST, "Spring-detected bad request"),
+    VALIDATION_ERROR(10002, HttpStatus.BAD_REQUEST, "Validation error"),
+    NOT_FOUND(10003, HttpStatus.NOT_FOUND, "Requested resource is not found"),
+    INTERNAL_ERROR(20000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal error"),
+    SPRING_INTERNAL_ERROR(20001, HttpStatus.INTERNAL_SERVER_ERROR, "Spring-detected internal error"),
+    DATA_ACCESS_ERROR(20002, HttpStatus.INTERNAL_SERVER_ERROR, "Data access error");
 
     private final Integer code;
-    private final ErrorCategory errorCategory;
+    private final HttpStatus httpStatus;
     private final String message;
+
+    public static ErrorCode valueOf(HttpStatus httpStatus) {
+        if (httpStatus == null) {
+            throw new GeneralException("HttpStatus is null.");
+        }
+
+        return Arrays.stream(values()).filter(errorCode ->
+                        errorCode.getHttpStatus() == httpStatus)
+                .findFirst()
+                .orElseGet(() -> {
+                    if (httpStatus.is4xxClientError()) {
+                        return ErrorCode.BAD_REQUEST;
+                    } else if (httpStatus.is5xxServerError()) {
+                        return ErrorCode.INTERNAL_ERROR;
+                    } else {
+                        return ErrorCode.OK;
+                    }
+                });
+    }
 
     public String getMessage(Throwable e) {
         return getMessage(this.getMessage() + " - " + e.getMessage());
@@ -33,20 +55,8 @@ public enum ErrorCode {
                 .orElse(this.getMessage());
     }
 
-    public boolean isClientSideError() {
-        return this.getErrorCategory() == ErrorCategory.CLIENT_SIDE;
-    }
-
-    public boolean isServerSideError() {
-        return this.getErrorCategory() == ErrorCategory.SERVER_SIDE;
-    }
-
     @Override
     public String toString() {
         return String.format("%s (%d)", this.name(), this.getCode());
-    }
-
-    private enum ErrorCategory {
-        NORMAL, CLIENT_SIDE, SERVER_SIDE
     }
 }
