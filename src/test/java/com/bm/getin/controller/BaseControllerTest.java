f0065216ce3f0a3545dfbd71ec1af6aa70454d77
bmcho <bmcho91@naver.com>
2022-08-26 18:11:21 +0900

    Refactor #18 - some features
    * 확인 페이지: 자동 로딩을 테스트 관찰하기 위해 5초로 했는데 테스트가 충분하므로 1.5초로 낮춤
    * 어드민 장소 상세 페이지: 이벤트 리스트는 수정 시에만 보이도록 `th:if` 추가
    * 테스트에 불필요한 출력, import 삭제

--- src/test/java/com/bm/getin/controller/BaseControllerTest.java
+++ src/test/java/com/bm/getin/controller/BaseControllerTest.java
@@ -8,12 +8,9 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 기본 페이지")
